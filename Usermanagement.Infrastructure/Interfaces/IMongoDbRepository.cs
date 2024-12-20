ca2fc4b231766c5d0fa35b37a3f2f2cd49d45d5a
Beyramzade <a.beyramzadeh78@gmail.com>
2024-12-20 21:04:08 +0330

    add mongo db repository

---
+++ Usermanagement.Infrastructure/Interfaces/IMongoDbRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Usermanagement.Infrastructure.Interfaces
+{
+    internal interface IMongoDbRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(string id, T entity);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ Usermanagement.Infrastructure/Repository/MongoRepository.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using Usermanagement.Infrastructure.Interfaces;
+
+public class MongoRepository<T> : IMongoDbRepository<T> where T : class
+{
+    private readonly IMongoCollection<T> _collection;
+
+    public MongoRepository(IMongoDatabase database, string collectionName)
+    {
+        _collection = database.GetCollection<T>(collectionName);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _collection.Find(Builders<T>.Filter.Empty).ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(string id)
+    {
+        return await _collection.Find(Builders<T>.Filter.Eq("_id", id)).FirstOrDefaultAsync();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await _collection.InsertOneAsync(entity);
+    }
+
+    public async Task UpdateAsync(string id, T entity)
+    {
+        await _collection.ReplaceOneAsync(Builders<T>.Filter.Eq("_id", id), entity);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _collection.DeleteOneAsync(Builders<T>.Filter.Eq("_id", id));
+    }
+}
